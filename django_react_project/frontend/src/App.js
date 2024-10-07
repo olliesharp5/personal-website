229e5758edca6a5bc10535fadc9e90a86aed15e5
Oliver Sharp <olliesharp5@gmail.com>
2024-10-07 20:29:09 +0200

    Updated Switch syntax for Routes

--- django_react_project/frontend/src/App.js
+++ django_react_project/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
@@ -20,13 +20,13 @@ const App = () => {
           </ul>
         </nav>
 
-        <Switch>
+        <Routes>
           <Route exact path="/" component={Home} />
           <Route path="/projects" component={Projects} />
           <Route path="/skills" component={Skills} />
           <Route path="/experience" component={Experience} />
           <Route path="/contact" component={Contact} />
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );

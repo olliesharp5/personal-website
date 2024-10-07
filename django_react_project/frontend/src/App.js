4de8b9c8d114c5ecb7ac14b69a5e452f0968891e
Oliver Sharp <olliesharp5@gmail.com>
2024-10-07 20:17:34 +0200

    Set up routing and navigation in App.js

--- django_react_project/frontend/src/App.js
+++ django_react_project/frontend/src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import ItemList from './components/ItemList';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <ItemList />
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/skills">Skills</Link></li>
+            <li><Link to="/experience">Experience</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;

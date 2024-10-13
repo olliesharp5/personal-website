64c862137075b8ff2195d7b774b8d8a50da51fb2
Oliver Sharp <olliesharp5@gmail.com>
2024-10-13 16:28:24 +0200

    Set up Postgresql database and linked to backend

--- django_react_project/backend/backend/settings.py
+++ django_react_project/backend/backend/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mypersonalwebsite_db',
+        'USER': 'default_user',
+        'PASSWORD': os.getenv('POSTRESQL_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 

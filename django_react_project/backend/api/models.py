3d458e57856d28cc548dd46f1cbb835943d04ff8
Oliver Sharp <olliesharp5@gmail.com>
2024-10-13 19:14:04 +0200

    Added model for contact form

--- django_react_project/backend/api/models.py
+++ django_react_project/backend/api/models.py
@@ -79,3 +79,14 @@ class Role(models.Model):
     class Meta:
         verbose_name = "Position"
         verbose_name_plural = "Positions"
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name} - {self.subject}"

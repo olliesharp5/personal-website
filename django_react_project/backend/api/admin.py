2d25fdbfbb202ecf8fbfcfebc94076c23e1b50dd
Oliver Sharp <olliesharp5@gmail.com>
2024-10-13 17:08:11 +0200

    Added ProjectImageInline to allow multiple image uploads in the Django admin panel

--- django_react_project/backend/api/admin.py
+++ django_react_project/backend/api/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Skill, Project, ProjectImage
 
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1  # Number of empty forms to display initially
+    max_num = 10  # Maximum number of images to upload
+
+# Register the Project model with the inline ProjectImage form
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ProjectImageInline]
+
 # Register your models here.
 admin.site.register(Skill)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(ProjectImage)

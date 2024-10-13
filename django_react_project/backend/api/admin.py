668d0cf47baa721c4a3d25048bbe30ccc1bab1c1
Oliver Sharp <olliesharp5@gmail.com>
2024-10-13 18:58:31 +0200

    Updated admin for models to display, added inline forms to add roles inside of company model

--- django_react_project/backend/api/admin.py
+++ django_react_project/backend/api/admin.py
@@ -1,16 +1,25 @@
 from django.contrib import admin
-from .models import Skill, Project, ProjectImage
+from .models import Skill, Project, ProjectImage, Company, Role
 
 class ProjectImageInline(admin.TabularInline):
     model = ProjectImage
-    extra = 1  # Number of empty forms to display initially
+    extra = 1
     max_num = 10  # Maximum number of images to upload
 
 # Register the Project model with the inline ProjectImage form
 class ProjectAdmin(admin.ModelAdmin):
     inlines = [ProjectImageInline]
 
+class RoleInline(admin.TabularInline):
+    model = Role
+    extra = 1
+
+class CompanyAdmin(admin.ModelAdmin):
+    inlines = [RoleInline]
+
 # Register your models here.
 admin.site.register(Skill)
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(ProjectImage)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Role)

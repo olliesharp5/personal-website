3571e270b2caf0398fee842b1b4b676658170433
Oliver Sharp <olliesharp5@gmail.com>
2024-10-18 18:43:58 +0200

    Added content to home screen

--- django_react_project/frontend/src/components/Home.js
+++ django_react_project/frontend/src/components/Home.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Home = () => (
   <div className="content">
-    <h1>Welcome to My Personal Website</h1>
-    <p>This is the introduction section where you can share some details about yourself.</p>
+    <h1>Oliver Sharp</h1>
+    <h2>Full Stack Software Developer</h2>
+    <h4>Welcome to my personal website! I'm Oliver Sharp, a Junior Full Stack Software Developer with a passion for creating impactful digital solutions. After earning my Diploma in Full Stack Software Development from the Code Institute, I transitioned from a successful career in fintech business operations, where I honed my problem-solving, communication, and project management skills.</h4>
+    <h4>Here, you’ll find my portfolio of projects, showcasing my expertise in HTML, CSS, JavaScript, Python, Django, React, and more. I’m excited to take on new challenges and contribute to innovative teams, aiming to build a career in software development that bridges the gap between technology and business success.</h4>
+    <h4>Feel free to explore my work, learn more about my journey, and get in touch for collaborations or opportunities.</h4>
   </div>
 );
 

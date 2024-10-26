ed88c960926d9fd7cff237885f1d1846e1603bbd
Oliver Sharp <olliesharp5@gmail.com>
2024-10-26 15:09:55 +0200

    Added linkedin and github links to home page

--- django_react_project/frontend/src/components/Home.js
+++ django_react_project/frontend/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import placeholderImage from '../images/placeholder-image.jpg';
 
 const Home = () => {
@@ -20,6 +21,34 @@ const Home = () => {
     <div className="content-container">
       {/* Hero Section */}
       <motion.div className="hero-section">
+
+        {/* Icons Div */}
+        <motion.div
+          className="hero-icons"
+          initial={{ x: 50, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.5, duration: 1 }}
+        >
+          <motion.a
+            href="https://github.com/olliesharp5"
+            target="_blank"
+            rel="noopener noreferrer"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <FaGithub size={40} />
+          </motion.a>
+          <motion.a
+            href="https://www.linkedin.com/in/oliver-sharp-3b4219201/"
+            target="_blank"
+            rel="noopener noreferrer"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <FaLinkedin size={40} />
+          </motion.a>
+        </motion.div>
+
         <motion.div
           className="hero-content"
           initial={{ opacity: 0 }}

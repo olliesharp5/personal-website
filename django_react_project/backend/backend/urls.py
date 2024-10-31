10a9c4afad100ee0718dba0eb5f4fa2705af457e
Oliver Sharp <olliesharp5@gmail.com>
2024-10-31 17:09:50 +0100

    Revert to backend-only setup

--- django_react_project/backend/backend/urls.py
+++ django_react_project/backend/backend/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # API routes
-    re_path(r'^.*$', views.index, name='index'),  # Catch-all for frontend
+    path('api/', include('api.urls')),
 ]
--- django_react_project/backend/backend/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-import os
-
-def index(request):
-    return render(request, 'index.html')

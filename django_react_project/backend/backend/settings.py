17d34b4fa841ae00da5657c91a5c32527f1cf7d6
Oliver Sharp <olliesharp5@gmail.com>
2024-10-31 16:45:04 +0100

    Continuous troubleshooting for heroku

--- django_react_project/backend/backend/settings.py
+++ django_react_project/backend/backend/settings.py
@@ -130,13 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'backend', 'staticfiles', 'build'),
+    os.path.join(BASE_DIR, 'backend', 'staticfiles', 'build', 'static'),
 ]
 
 # Default primary key field type

7a93a72248fcd8886b425612c7beab3a489cfa6e
Oliver Sharp <olliesharp5@gmail.com>
2024-11-02 18:22:29 +0100

    Updated contact page text

--- django_react_project/frontend/src/components/Contact.js
+++ django_react_project/frontend/src/components/Contact.js
@@ -8,10 +8,10 @@ const Contact = () => (
   <div className="content">
     <h1>Contact me!</h1>
     <h4>
-      Interested in working together or have questions? Contact me on LinkedIn or fill out this form to send me a direct email.
+      Interested in working together or have questions? Connect and contact me on <a href="https://www.linkedin.com/in/oliver-sharp-3b4219201/" target="_blank" rel="noopener noreferrer" className='contact-link'>LinkedIn</a> or fill out this form to send me a direct email.
     </h4>
     <h4>
-      I'm open to new opportunities and collaborations, and I'd love to hear from you.
+      I'm open to new opportunities and collaborations, and I'd love to hear from you!
     </h4>
     <ContactForm />
     <ToastContainer />

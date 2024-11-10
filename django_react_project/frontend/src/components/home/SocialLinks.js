59d54c87b8f03e6b4e90eb5bcc981b752a5dda2c
Oliver Sharp <olliesharp5@gmail.com>
2024-11-10 23:36:29 +0100

    Added memo to social links to aid loading times

--- django_react_project/frontend/src/components/home/SocialLinks.js
+++ django_react_project/frontend/src/components/home/SocialLinks.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { motion } from 'framer-motion';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
-const SocialLinks = () => (
+const SocialLinks = memo(() => (
   <motion.div className="hero-icons" initial={{ x: 50, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ delay: 0.5, duration: 1 }}>
     <motion.a href="https://github.com/olliesharp5" target="_blank" rel="noopener noreferrer" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
       <FaGithub size={40} />
@@ -11,6 +11,6 @@ const SocialLinks = () => (
       <FaLinkedin size={40} />
     </motion.a>
   </motion.div>
-);
+));
 
 export default SocialLinks;
